a7d0949bf1ca2219ba92db49a086dfbb4fa41c7f
Zeljko <ikanoviczeljko095@gmail.com>
2025-02-23 00:34:05 +0100

    fix

--- API/Services/Api/ApiService.cs
+++ API/Services/Api/ApiService.cs
@@ -15,8 +15,9 @@ namespace API.Services.Api
         public ApiService(HttpClient httpClient, IConfiguration configuration,UserManager<User> userManager)
         {
             _httpClient = httpClient;
-            _flaskApiUrl1 = configuration["FlaskApi:Url"];  // Učitavanje URL-a iz konfiguracije
+            _flaskApiUrl1 = configuration["FlaskApi:Url1"];  // Učitavanje URL-a iz konfiguracije
             _flaskApiUrl2 = configuration["FlaskApi:Url2"];
+            _flaskApiUrl3= configuration["FlaskApi:Url3"];
             _userManager= userManager;
         }
 
@@ -53,7 +54,71 @@ namespace API.Services.Api
             var content = new StringContent("{}", Encoding.UTF8, "application/json");
          
             
+            var response = await _httpClient.GetAsync(_flaskApiUrl1+userId);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                // Deserializacija odgovora u anonimni objekat
+                var responseObject = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                // Dobijanje imena i uklanjanje nepotrebnih karaktera
+                string ime = responseObject.ToString().Trim('"', '\n');
+
+                return ime;
+            }
+            else
+            {
+                return "Greška: " + response.StatusCode;
+            }
+        }
+        catch (Exception ex)
+        {
+            return "Izuzetak: " + ex.Message;
+        }
+        }
+
+         public async Task<string> GameQuizzPlayAsync(string userId)
+        {
+            try
+        {
+            var content = new StringContent("{}", Encoding.UTF8, "application/json");
+         
+            
             var response = await _httpClient.GetAsync(_flaskApiUrl2+userId);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                // Deserializacija odgovora u anonimni objekat
+                var responseObject = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                // Dobijanje imena i uklanjanje nepotrebnih karaktera
+                string ime = responseObject.ToString().Trim('"', '\n');
+
+                return ime;
+            }
+            else
+            {
+                return "Greška: " + response.StatusCode;
+            }
+        }
+        catch (Exception ex)
+        {
+            return "Izuzetak: " + ex.Message;
+        }
+        }
+
+         public async Task<string> GameLeftPlayAsync(string userId)
+        {
+            try
+        {
+            var content = new StringContent("{}", Encoding.UTF8, "application/json");
+         
+            
+            var response = await _httpClient.GetAsync(_flaskApiUrl3+userId);
 
             if (response.IsSuccessStatusCode)
             {
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2705dd48cfccd1cb55d657c818ce5f461cf9183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7862384f180c895d64382678d3d5b18fad2c071b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
